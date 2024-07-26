d4eb1cefe88802e5687cf0e3e32f1414acdfd134
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-07-26 11:37:07 -0700

    StarBackground 1

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
         autoPlay
         muted
         loop
-        className='rotate-180 absolute top-[-340px] left-o z-[1] w-full h-full object-cover'
+        className='rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover'
         >
           <source src='./blackhole.webm' type='video/webm'/>
         </video>
--- components/main/sub/HeroContent.tsx
+++ components/main/sub/HeroContent.tsx
@@ -17,7 +17,7 @@ export const HeroContent = () => {
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
             <motion.div 
             variants={slideInFromTop}
-            className='Welcome-box py-[-5px] px-[10px] border border-[#7042f88b] opacity-[0.9]'
+            className='Welcome-box py-[-5px] px-[10px] border border-[#7042f88b] opacity-90'
             >
               <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/>
               <h1 className='Welcome-text text-[13px]'>
@@ -26,13 +26,13 @@ export const HeroContent = () => {
             </motion.div>
             <motion.div
             variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
+            className='flex flex-col gap-6 mt-6 text-5xl font-bold text-white max-w-[600px] w-auto h-auto'
             >
-              <span>
-              Providing
-              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
-              project experience
-              </span>
+              
+              
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>Veera Vivek Telagani</span>
+              Software Engineer
+              
             </motion.div>
 
             <motion.p
